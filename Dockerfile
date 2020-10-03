2e55a1a30a974555cd5204f7994e235550d91875
Tomasz Wiewióra <tomasz.wiewiora95@gmail.com>
2020-10-03 16:01:18 +0200

    fix v5

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,5 @@ FROM python:3.7-alpine
 WORKDIR /app
 COPY requirements.txt requirements.txt
 RUN pip install -r requirements.txt
-EXPOSE 5000
 COPY . .
 CMD ["python", "wsgi.py"]
--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,7 @@
 import os
-
 from app.main import app
 
+port = int(os.environ.get("PORT", 5000))
+
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8080)
+    app.run(debug=True, host='0.0.0.0', port=port)
